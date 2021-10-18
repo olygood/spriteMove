26a594ee4ef5b84edacbaee667a8f9377f29f45b
olygood <olivier.huttmacher@gmail.com>
2021-10-18 17:54:07 +0200

    delta time fin avec requestAnimationFrame(run)plus precis et ajustement des fps

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
-
+//Variables-----------------------------------------
 const canva = document.getElementById('canva');
 let ctx = canva.getContext('2d');
 let interval;
-let lastUpdate = Date.now();
+let lastUpdate = 0;
+let FPS = 0;
 
 canva.width = 400
 canva.height = 400
-// let interval;
-// let fps = 0;
-// let dernierUpdate = 0;
-function run()
+//fonction game-----------------------------------
+//la fonction requestAnimationFrame renvoi un timeframe donc le capturer dans la fonction comme parametre
+function run(timeFrame)
 {
-    let atPresent = Date.now();
-    let deltaTime = (atPresent - lastUpdate) /1000;
-    lastUpdate = atPresent;
+    requestAnimationFrame(run);
 
+    let deltaTime = (timeFrame- lastUpdate) /1000;
+    //astuce pour limiter a 30fps : explosion ralentissement du jeu 
+    //une fois le delta time calculer : le 0.001 c'est pour fignoler l'ajustement
+    //  if(deltaTime<(1/30 - 0.001))
+    //  {return;}
+    lastUpdate = timeFrame;
+    FPS = 1/deltaTime;
     update(deltaTime);
     ctx.clearRect(0,0,canva.width,canva.height);
     draw(ctx);
+    showFPS();
 }
+
 function init()
 {
-    
     load();
-    interval = setInterval(run,1000/60);
+    requestAnimationFrame(run);
 }
 init();
+
+function showFPS()
+{
+    ctx.fillStyle ='black';
+    ctx.font = "normal 16pt Arial";
+    ctx.fillText(Math.floor(FPS) + "fps" ,10,20);
+}
--- sprite.js
+++ sprite.js
@@ -9,3 +9,4 @@ class Sprite {
         pCtx.drawImage(this.img,this.x,this.y);
     }
 }
+
